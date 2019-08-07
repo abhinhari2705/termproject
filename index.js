dd3361f04f5e9b225b366d9fb61a099b64330363
Abhin Hari <755176@student.tor.loc>
2019-08-07 17:18:04 -0400

    5:17 commit

--- index.js
+++ index.js
@@ -1,9 +1,26 @@
-const http =require('http');
-const site = http.createServer(function(req,res){
-    console.log("hello abhi")
-    res.setHeader('Content-type','text/html');
-    console.log(req.rawHeaders);
-    res.end('<h1> Hello class</h1>');
-});
+var express = require('express');
+var app = express();
+const bodyParser = require('body-parser');
+app.use(bodyParser.urlencoded({extended:false}))
+app.use(bodyParser.json());
+app.use(function (req,res,next){
+    console.log("i amin the middle");
+    console.log(req.body);
+    next();
+    
 
-site.listen(3000);
+    })
+app.get('/',function (req,res){
+    res.send('<h1>get hello world</h1>')
+});
+app.post('/',function (req,res){
+    console.log(reg.body)
+    res.send('<h1>post sent</h1>')
+});
+app.put('/',function (req,res){
+    res.send('PUT send')
+});
+app.delete('/',function (req,res){
+    res.send('DELETE sent')
+});
+app.listen(3000);
